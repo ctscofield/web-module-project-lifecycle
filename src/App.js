3bd7cf4f337d31e3db8abe8037b573bc2f1ebf93
ctscofield <ctscofield@gmail.com>
2021-05-04 16:33:02 -0600

    rendering to the page, just my profile info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    me: [],
+    friends: []
+  }
+
+  componentDidMount() {
+    axios
+      .get(`https://api.github.com/users/ctscofield`)
+      .then(res=> {
+        console.log(res.data)
+        this.setState({
+          me: res.data
+        });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My Github and friends</h1>
+        <div>
+          <img src={this.state.me.avatar_url} alt="profile avatar" style={{width: '180px', height: '240px'}} />
+          <h2>{this.state.me.name}</h2>
+          <h4>Handle: {this.state.me.login}</h4>
+          <p>{this.state.me.location}</p>
+          <a href={this.state.me.html_url}>Profile</a>
+          
+
+        </div>
+      </div>
+    );
+  }  
 }
 
 export default App;
